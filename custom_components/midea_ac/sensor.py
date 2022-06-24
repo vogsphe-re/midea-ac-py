0e39525375f6cb57490dc55833887327517a1e85
Tucker Kern <tuckkern@gmail.com>
2022-06-23 21:35:47 -0600

    Create a better name for the sensor platforms

--- custom_components/midea_ac/sensor.py
+++ custom_components/midea_ac/sensor.py
@@ -17,6 +17,7 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
+
 async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
@@ -25,7 +26,7 @@ async def async_setup_entry(
     """Setup the sensor platform for Midea Smart AC."""
 
     _LOGGER.info("Setting up sensor platform.")
-    
+
     # Get config data from entry
     config = config_entry.data
 
@@ -35,18 +36,16 @@ async def async_setup_entry(
 
     # Create sensor entities from device
     add_entities([
-        MideaTemperatureSensor(
-            device, "Indoor Temperature", "indoor_temperature"),
-        MideaTemperatureSensor(
-            device, "Outdoor Temperature", "outdoor_temperature"),
+        MideaTemperatureSensor(device, "indoor_temperature"),
+        MideaTemperatureSensor(device, "outdoor_temperature"),
     ])
+
 
 class MideaTemperatureSensor(RestoreSensor):
     """Temperature sensor for Midea AC."""
 
-    def __init__(self, device, friendly_name, prop):
+    def __init__(self, device, prop):
         self._device = device
-        self._name = friendly_name
         self._prop = prop
         self._native_value = None
 
@@ -74,8 +73,7 @@ class MideaTemperatureSensor(RestoreSensor):
 
     @property
     def name(self) -> str:
-        # TODO better names
-        return f"{self._name}"
+        return f"{DOMAIN}_{self._prop}"
 
     @property
     def unique_id(self) -> str:
